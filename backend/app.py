f6f20f9f35f121001e2d334d1c3451dd6c3a8383
Anik Chand <anikchand461@gmail.com>
2026-07-30 18:25:30 +0530

    add favicon

--- backend/app.py
+++ backend/app.py
@@ -21,10 +21,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://127.0.0.1:5501",
+        "https://chatwithrepo.vercel.app/",
         "http://localhost:5500",
-        "http://localhost:5501",
         "null",
     ],
     allow_credentials=True,
